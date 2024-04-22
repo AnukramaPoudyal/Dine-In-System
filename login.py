37b59f3528d64478321d7cd9f68443dc49ea0afe
subikshya-koirala <119571931+subikshya-koirala@users.noreply.github.com>
2024-04-22 09:05:02 +0545

    Update login.py

--- login.py
+++ login.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+import re
+from django.db import models
 from django.views.decorators.csrf import csrf_exempt
 import json
-from django.contrib.auth.hashers import make_password, check_password
-from .models import User
+import sample.user.models as usermodel
 
 @csrf_exempt
 def login(request):
@@ -11,24 +12,14 @@ def login(request):
         data = json.loads(request.body)
         email = data.get("email")
         password = data.get("password")
-        
-        if not email or not password:
-            return JsonResponse({"error": "Invalid parameters"}, status=400)
+        if is_valid_email(email):
+            user = usermodel.User.objects.get(email=email,password=password)
+            print(user)
+            if user is None:
+                return JsonResponse({"error": "No user exists"})
+            # connect with database for checking valid details
+            # For simplicity, let's just return a success message
+            return JsonResponse({"message": "Login successful"})
+        return  JsonResponse({"error": "Invalid-email"})
 
-        try:
-            user = User.objects.get(email=email)
-        except User.DoesNotExist:
-            return JsonResponse({"error": "User does not exist"}, status=400)
-
-        if not check_password(password, user.password):
-            return JsonResponse({"error": "Invalid credentials"}, status=400)
-
-        if not user.is_active:
-            return JsonResponse({"error": "User account is not active"}, status=400)
-
-        if not user.signup_completed:
-            return JsonResponse({"error": "User signup process is not completed"}, status=400)
-
-        return JsonResponse({"message": "Login successful"})
-    
-    return JsonResponse({"error": "Invalid method"}, status=405)
+    return JsonResponse({"error": "Invalid-parameters"})
