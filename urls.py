eff9d65a32a5bd04641da93f6f9c56864f007443
AnukramaPoudyal <87685960+AnukramaPoudyal@users.noreply.github.com>
2024-05-08 23:38:59 +0545

    Delete urls.py

--- urls.py
+++
@@ -1,9 +0,0 @@
-# signup/urls.py
-
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('login/', views.login, name='login'),
-    path('signup/', views.signup, name='signup'),
-]
