1c4c3758deaac3391ea320f957c7116ff62c7f03
अनुक्रमा पौड्याल <87685960+AnukramaPoudyal@users.noreply.github.com>
2024-05-09 11:16:49 +0545

    Update booking.js

--- Frontend/JS/booking.js
+++ Frontend/JS/booking.js
@@ -1,26 +1,25 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const form = document.querySelector('form');
-
-    form.addEventListener('submit', function (event) {
+    // Handle form submission for table reservation
+    document.getElementById("reservation-form").addEventListener("submit", function(event) {
         event.preventDefault(); // Prevent default form submission
 
         // Name validation
-        const firstName = form.querySelector('#first_name').value.trim();
-        const lastName = form.querySelector('#last_name').value.trim();
+        const firstName = this.querySelector('#first_name').value.trim();
+        const lastName = this.querySelector('#last_name').value.trim();
         if (!isValidName(firstName) || !isValidName(lastName)) {
             alert('Please enter a valid first and last name.');
             return;
         }
 
         // Email validation
-        const email = form.querySelector('#email').value.trim();
+        const email = this.querySelector('#email').value.trim();
         if (!isValidEmail(email)) {
             alert('Please enter a valid email address.');
             return;
         }
 
         // Phone number validation
-        const phoneNumber = form.querySelector('#phone_number').value.trim();
+        const phoneNumber = this.querySelector('#phone_number').value.trim();
         if (!isValidPhoneNumber(phoneNumber)) {
             alert('Please enter a valid phone number.');
             return;
@@ -29,6 +28,11 @@ document.addEventListener('DOMContentLoaded', function () {
         // Redirect to payment.html
         window.location.href = "payment.html";
     });
+
+    // Function to handle cancellation of booking
+    window.cancelBooking = function() {
+        alert("Booking Cancelled. Hope you have a great day ahead!");
+    }
 });
 
 function isValidName(name) {
