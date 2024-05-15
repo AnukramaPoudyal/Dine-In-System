22ce2f01210e70bb1b58a2506081b75083ea7f46
yugeenzsm <166278371+yugeenzsm@users.noreply.github.com>
2024-05-15 22:48:48 +0545

    Update urls.py

--- Backend/sample/main/urls.py
+++ Backend/sample/main/urls.py
@@ -4,10 +4,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.login, name='login'),
-    path('signup/', views.signup, name='signup'),
-    path('dashboard/', views.dashboard, name='dashboard'),
-    path('register/', views.add_registration, name='add_registration'), 
-    path('list/', views.list_registrations, name='list_registrations'),
-
+    path("login/", views.login, name="login"),
+    path("signup/", views.signup, name="signup"),
+    path("dashboard/", views.dashboard, name="dashboard"),
+    path('summary/', views.dashboard_summary, name='dashboard_summary'),
+    path("find-reservation/", views.find_reservation, name="find_reservation"),  # Example path
+    path("reserve/", views.add_reservation, name="add_reservation"),  # Example path
+    path("list/", views.list_reservations, name="list_reservations"),
+    path("list-all/", views.list_all_reservations, name="list_all_reservations"),
+    path("add-table/", views.add_table, name="add_table"),
 ]
+
